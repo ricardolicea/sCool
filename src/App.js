ea901d100b54905f3c9a0b861a2b6543ae0ef443
ricardolicea <ricardo.liceamata@gmail.com>
2024-05-11 12:47:06 -0600

    Created TabButton Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import TabButton from './components/TabButton';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <section id="menu">
+        <TabButton> Students </TabButton>
+        <TabButton> Teachers </TabButton>
+        <TabButton> Groups </TabButton>
+      </section>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,6 +24,8 @@ function App() {
           Learn React
         </a>
       </header>
+      
+
     </div>
   );
 }
---
+++ src/components/TabButton.jsx
@@ -0,0 +1,7 @@
+export default function TabButton ({children}){
+    return (
+       <li>
+        <button className="">{children}</button>
+       </li>
+    )
+}
